72913cad863330152a16432e7acded867bc4b01d
Brendan O'Connor <ussjoin@ussjoin.com>
2011-12-03 15:24:41 -0600

    Checking in Rails app and the DotCloud configuration files.

---
+++ dashboard/frontend/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Frontend::Application.config.secret_token = '25d7e8e1bf41bfe66f6ae0e89b048c78e92032627f905497010c6f1811915d41f60dd63c514fea15c23b8d311bf4de4eaf2bdf828e2b93af645b54e51546c424'
