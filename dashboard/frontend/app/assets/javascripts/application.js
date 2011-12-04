da5b5167d58ca8dc953718ba004c257f0aa253b4
unknown <Bob@minibob.(none)>
2011-12-03 20:28:33 -0600

    Adding libraries and doing some style updates.

--- dashboard/frontend/app/assets/javascripts/application.js
+++ dashboard/frontend/app/assets/javascripts/application.js
@@ -7,3 +7,13 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+$(document).ready( function () {
+	$('.datatable').dataTable({
+        "bPaginate": false,
+        "bLengthChange": false,
+        "bFilter": true,
+        "bSort": true,
+        "bInfo": false,
+        "bAutoWidth": false
+    } );
+});
